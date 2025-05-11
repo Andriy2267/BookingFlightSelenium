686fe3e7eeae3f9ed39c52587b05aebe75cff726
Andriy2267 <andrijcopek696@gmail.com>
2025-05-11 09:49:16 +0300

    add searching flight functionality + closing pop-up

--- bookingFlight/booking_flight.py
+++ bookingFlight/booking_flight.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
-from bookingFlight.constants import URL_BASE
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+from bookingFlight.constants import URL_BASE
+from bookingFlight.pages.home_page import HomePage
+from bookingFlight.pages.flight_search_page import FlightSearchPage
 from .captcha_solver import solve_funcaptcha
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 class BookingFlight(webdriver.Chrome):
 
@@ -15,98 +18,40 @@ class BookingFlight(webdriver.Chrome):
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_argument("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.10 Safari/605.1.1")
         if detach:
-             options.add_experimental_option("detach", True)
- 
+            options.add_experimental_option("detach", True)
+
         service = Service(ChromeDriverManager().install())
-        super().__init__(service=service, options=options)
-        self.maximize_window()
-    
+        self.driver = webdriver.Chrome(service=service, options=options)
+        self.driver.maximize_window()
 
     def __del__(self):
         if self.teardown:
-            self.quit()
-
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.teardown:
-            self.quit()
-
+            self.driver.quit()
 
     def land_first_page(self):
-        self.get(URL_BASE)
+        self.driver.get(URL_BASE)
 
+    def search_flight(self, origin="LHR", destination="FRA"):
+        homepage = HomePage(self.driver)
+        homepage.go_to_flights()
+        flight_page = FlightSearchPage(self.driver)
+        flight_page.select_route(origin, destination)
 
-    def open_flight(self):
-        wait = WebDriverWait(self, 5)
-        select_fligth_element = wait.until(EC.element_to_be_clickable((By.ID, "flights")))
-        select_fligth_element.click()
+    def close_optional_popup(self):
+        try:
+            popup = WebDriverWait(self, 3).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "bbe73dce14"))
+            )
+            try:
+                close_button = popup.find_element(By.CSS_SELECTOR, 'span[class="ec1ff2f0cb"]')
+                close_button.click()
+            except NoSuchElementException:
+                WebDriverWait(self, 5).until(
+                    EC.invisibility_of_element_located((By.CLASS_NAME, "bbe73dce14"))
+                )
+        except TimeoutException:
+            pass
 
-
-    def change_language(self, language="English (US)"):
-        wait = WebDriverWait(self, 5)
-        select_language_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-testid="header-language-picker-trigger"]')))
-        select_language_button.click()
-
-        for language_option in self.find_elements(By.CSS_SELECTOR, 'button[data-testid="selection-item"]'):
-            if language in language_option.text:
-                language_option.click()
-                break
-
-    
-    def change_currency(self, currency="USD"):
-        wait = WebDriverWait(self, 5)
-        select_currency_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-testid="header-currency-picker-trigger"]')))
-        select_currency_button.click()
-
-        for currency_option in self.find_elements(By.CSS_SELECTOR, 'button[data-testid="selection-item"]'):
-            if currency in currency_option.text:
-                currency_option.click()
-                break
-
-
-    def select_flight_type(self, type="One-way"):
-        wait = WebDriverWait(self, 5)
-        if type == "One-way":
-            select_oneway_type = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div[data-ui-name="search_type_oneway"]')))
-            select_oneway_type.click()
-        elif type == "Round-trip":
-            select_roundtrip_type = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div[data-ui-name="search_type_roundtrip"]')))
-            select_roundtrip_type.click()
-        else:
-            select_multicity_type = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div[data-ui-name="search_type_multistop"]')))
-            select_multicity_type.click()
-
-    
-    def select_flight_option(self, option="Economy"):
-        wait = WebDriverWait(self, 5)
-        select_option = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div[class="Select-module__wrapper___Aa7YD"]')))
-        select_option.click()
-
-        if option == "Economy":
-            select_economy_option = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'option[value="ECONOMY"]')))
-            select_economy_option.click()
-        elif option == "Premium economy":
-            select_premiumeconomy_option = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'option[value="PREMIUM_ECONOMY"]')))
-            select_premiumeconomy_option.click()
-        elif option == "Business":
-            select_business_option = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'option[value="BUSINESS"]')))
-            select_business_option.click()
-        else:
-            select_firstclass_option = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'option[value="FIRST"]')))
-            select_firstclass_option.click()
-
-    
-    def select_direct_flight(self, direct_flight=True):
-        wait = WebDriverWait(self, 5)
-        if direct_flight:
-            select_direct_flight = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div[data-ui-name="direct_flights"]')))
-            select_direct_flight.click()
-
-
-
-
-                
-
-                                          
-
-    
+    def quit(self):
+        if self.teardown:
+            self.driver.quit()
--- run.py
+++ run.py
@@ -3,10 +3,15 @@ from bookingFlight.booking_flight import BookingFlight
 with BookingFlight(teardown=False) as bot:
     bot.land_first_page()
     # bot.bypass_captcha()
+    bot.close_optional_popup()
     bot.change_currency()
+    bot.close_optional_popup()
     bot.open_flight()
+    bot.close_optional_popup()
     bot.change_language()
+    bot.close_optional_popup()
     bot.select_flight_type()
     bot.select_flight_option(option="First-class")
-    bot.select_direct_flight(direct_flight=False)
+    bot.select_direct_flight(direct_flight=True)
+    bot.select_outbound_flight()
 
